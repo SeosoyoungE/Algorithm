a8ab4c99d370a91ea1adab2c11934f50490b802a
SeosoyoungE <127925743+SeosoyoungE@users.noreply.github.com>
2023-04-04 00:18:23 +0900

    [unrated] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/unrated/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,8 +1,8 @@
 -- 코드를 입력하세요
--- select * from BOOK where YEAR(SALES_DATE)=2022 and month(SALES_DATE)=1;
- SELECT CATEGORY, sum(sales)
- from BOOK b
- join BOOK_SALES s on b.book_id=s.book_id
- where YEAR(SALES_DATE)=2022 and month(SALES_DATE)=1 and 
- (CATEGORY,s.BOOK_ID,SALES_DATE,SALES) in (select CATEGORY,s.BOOK_ID,SALES_DATE,SALES from BOOK b
- join BOOK_SALES s on b.book_id=s.book_id where YEAR(SALES_DATE)=2022 and month(SALES_DATE)=1 group by b.Book_id) group by CATEGORY order by category;
+-- select b.BOOK_ID	,CATEGORY,SALES_DATE	,sum(SALES) from BOOK b join BOOK_SALES s on b.book_id=s.book_id where YEAR(SALES_DATE)=2022 and month(SALES_DATE)=1 group by b.Book_id;
+  SELECT CATEGORY, sum(sales)
+  from BOOK b
+  join BOOK_SALES s on b.book_id=s.book_id
+  where YEAR(SALES_DATE)=2022 and month(SALES_DATE)=1 and 
+  (CATEGORY,s.BOOK_ID,SALES_DATE,SALES) in (select CATEGORY,s.BOOK_ID,SALES_DATE,SALES from BOOK b
+  join BOOK_SALES s on b.book_id=s.book_id where YEAR(SALES_DATE)=2022 and month(SALES_DATE)=1 group by b.Book_id) group by CATEGORY order by category;
